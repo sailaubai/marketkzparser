e0d75c75d1a68689fb4ab805bde3f0bdb765c9a9
saylaubay <s.saylaubay@luxystech.com>
2018-11-20 11:31:35 +0600

    user serialize with amount of ads and rubrics

--- collect_users.py
+++ collect_users.py
@@ -6,7 +6,18 @@ import json
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 
-def rcollect_users():
+def collect_users():
+
+    def find(lst, key, val):
+        for i, dic in enumerate(lst):
+            if dic[key] == val:
+                return i
+        return -1
+
+    def user_serializer(user):
+        user['rubrics'] = list(user['rubrics'])
+        return user
+
     users = list()
     for cat, _ in task.categories:
         files_list = os.listdir(os.path.join(THIS_FOLDER, 'categories', cat))
@@ -17,12 +28,21 @@ def rcollect_users():
                     adverts = pickle.load(f)
                     for advert in adverts:
                         if isinstance(advert['user'], dict):
+                            newdict = advert['user']
+                            newdict['rubrics'] = {advert['category'], }
+                            newdict['amountOfAds'] = advert['price'] if advert['price'] else 0
                             users.append(advert['user'])
+                        elif advert['price']:
+                            index = find(users, 'name', advert['user'])
+                            if index >= 0:
+                                users[index]['amountOfAds'] += advert['price']
+                                users[index]['rubrics'].add(advert['category'])
                     f.close()
+    serializable_users = [user_serializer(user) for user in users]
     with open(os.path.join(THIS_FOLDER, 'users.json'), 'wb') as f:
-        s = json.dumps(users, sort_keys=True, indent=4, ensure_ascii=False).encode('utf8')
+        s = json.dumps(serializable_users, sort_keys=True, indent=4, ensure_ascii=False).encode('utf8')
         f.write(s)
 
 
 if __name__ == "__main__":
-    rcollect_users()
+    collect_users()
